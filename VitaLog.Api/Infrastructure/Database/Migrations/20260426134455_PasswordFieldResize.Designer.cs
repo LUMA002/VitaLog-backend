2e2ad68cd3653d126d357abf35d719ea4cfdc86c
Stanislav Malovaniuk <stasmalovaniuk@gmail.com>
2026-04-26 16:49:04 +0300

    feat: added migration (updated size of PasswordHash field to 256 from 1024)

--- VitaLog.Api/Infrastructure/Database/Migrations/20260426134455_PasswordFieldResize.Designer.cs
+++ VitaLog.Api/Infrastructure/Database/Migrations/20260426134455_PasswordFieldResize.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VitaLog.Api.Infrastructure.Database;
@@ -11,9 +12,11 @@ using VitaLog.Api.Infrastructure.Database;
 namespace VitaLog.Api.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260426134455_PasswordFieldResize")]
+    partial class PasswordFieldResize
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
